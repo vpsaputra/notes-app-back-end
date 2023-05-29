bc5b0177cf1f4251bc4ba5183b726f739355e536
vpsaputra <pikisaputra201@gmail.com>
2023-05-29 21:54:31 +0700

    Fix server host and addNotesHandler

--- src/handler.js
+++ src/handler.js
@@ -15,9 +15,28 @@ const addNoteHandler = (request, h) => {
     createdAt,
     updatedAt,
   };
+
   notes.push(newNote);
 
   const isSuccess = notes.filter((note) => note.id === id).length > 0;
+
+  if (isSuccess) {
+    const response = h.response({
+      status: "success",
+      message: "Catatan berhasil ditambahkan",
+      data: {
+        noteId: id,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+  const response = h.response({
+    status: "fail",
+    message: "Catatan gagal ditambahkan",
+  });
+  response.code(500);
+  return response;
 };
 
 const getAllNotesHandler = () => ({
